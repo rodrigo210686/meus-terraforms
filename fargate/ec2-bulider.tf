7881da5ff024aac68dd6b017c926815837c5714f
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-01-23 18:06:02 -0300

    Update ec2-bulider.tf

--- fargate/ec2-bulider.tf
+++ fargate/ec2-bulider.tf
@@ -39,45 +39,13 @@ resource "aws_iam_role" "ssm_role" {
         {
             "Effect": "Allow",
             "Action": [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-                "ssm:UpdateInstanceInformation"
+                "sts:AssumeRole"
             ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2messages:AcknowledgeMessage",
-                "ec2messages:DeleteMessage",
-                "ec2messages:FailMessage",
-                "ec2messages:GetEndpoint",
-                "ec2messages:GetMessages",
-                "ec2messages:SendReply"
-            ],
-            "Resource": "*"
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
         }
     ]
 }
