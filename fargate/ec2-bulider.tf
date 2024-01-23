2ce7bd5d47e6411ef86674c998cbcc84b9d70286
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-01-23 17:20:53 -0300

    Update ec2-bulider.tf

--- fargate/ec2-bulider.tf
+++ fargate/ec2-bulider.tf
@@ -1,3 +1,79 @@
+resource "aws_security_group" "app_builder_sg" {
+  name        = "sg-app-builder"
+  description = "App Builder Security Group"
+
+  ingress {
+    from_port = 3000
+    to_port   = 3000
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Add more ingress rules as needed
+}
+
+resource "aws_iam_role" "ssm_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  EOF
+}
+
+
 resource "aws_instance" "builder-app" {
     ami = "ami-0c0b74d29acd0cd97"
     instance_type = "t2.micro"
@@ -19,9 +95,11 @@ resource "aws_instance" "builder-app" {
               npm start
 
               EOF
-
+  iam_instance_profile = aws_iam_role.ssm_role.ec2-ssm-role
+  vpc_security_group_ids = [aws_security_group.app_builder_sg.id]
+  
   tags = {
-    Name = "builder-app"
-    type = "ec2"
+     Name = "builder-app"
+     type = "ec2"
   }
 }
