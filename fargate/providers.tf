c58e1afd29166826ef4bd02dddced5ce9f4c352f
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-01-29 17:15:24 -0300

    Update providers.tf

--- fargate/providers.tf
+++ fargate/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQBH6BVC6QSGFMHFZ"
-  secret_key = "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"
+  access_key = "AKIA2FMP5N76ADHFJUOF"
+  secret_key = "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"
 }
