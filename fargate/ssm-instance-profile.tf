c1721c576c7fb00b9c26e93c69ccf1f8f86d90e2
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-01-29 19:12:30 -0300

    Create ssm-instance-profile.tf

---
+++ fargate/ssm-instance-profile.tf
@@ -0,0 +1,28 @@
+### Create iam role intance profile
+resource "aws_iam_role" "ssm_role2" {
+  name = "acessossm2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "ssm-instance"
+
+  role = aws_iam_role.ssm_role2.name
+}
+### Atacch existent ssm core policie
+resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore22" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.ssm_role2.name
+}
