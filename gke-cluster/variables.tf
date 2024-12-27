fc340f7174a6bfe496c4a97d32396f3e657b5152
Rodrigo Barbosa <rodrigo2186@gmail.com>
2024-12-27 11:49:15 -0300

    gke cluster

---
+++ gke-cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+#variable "region" {}
+
+#variable "region" {
+#  description = "região"
+#  type        = string
+#}
